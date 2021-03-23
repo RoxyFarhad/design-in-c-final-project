a03a8e5a943777771ae8bdd6a4337f6d59a82990
RoxyFarhad <raf2192@columbia.edu>
2021-03-23 11:40:35 -0400

    first commit

---
+++ btree.h
@@ -0,0 +1,71 @@
+#ifndef BTREE_H
+#define BTREE_H
+
+#include <sys/types.h>
+#include <iterator>
+#include <iostream>
+#include <ostream> 
+#include <string> 
+
+namespace btree 
+{
+
+    template< typename Key, typename Data >
+    class BTree 
+    {
+
+        // typedefs for easier reference later
+        typedef Btree<Key, Data> btree_type; 
+        
+        typedef Iterator<Key, Data> iterator; 
+        typedef typename iterator::const_iteartor const_iterator;
+        typedef std::reverse_iterator< const_iterator> const_reverse_iterator; 
+        typedef std::reverse_iterator< iterator > iterator; 
+
+        /*
+            public methods
+        */
+        public:
+            Btree( ); // constructor
+            Btree( const btree_type &x ); // copy constructor 
+            ~Btree(); // destructor 
+
+            // iterator method headers
+            iterator begin(); 
+            const_iterator begin() const; 
+            iterator end(); 
+            const_iterator end() const; 
+            reverse_iterator rbegin(); 
+            const_reverse_iterator rbegin() const; 
+            reverse_iterator rend(); 
+            const_reverse_iterator rend() const; 
+
+            // functionality 
+            void clear();
+            void assign( const btree_type &x );
+
+        // ~~~~~ METHODS ~~~~~
+
+        // default constructor
+        template< typename Key, typename Data >
+        BTree< Key, Data>::Btree(): root(nullptr) {}
+        
+        // copy constructor
+        template< typename Key, typename Data > 
+        BTree< Key, Data >::Btree(const btree_type &x)
+        { 
+            // create a method to copy the tree using an iterator
+        }
+
+        // destructor 
+        template<typename Key, typename Data > 
+        Btree< Key, Data >::~Btree()
+        {
+            // create a method to delete the tree from memory
+        }
+
+    }
+
+}
+
+#endif
